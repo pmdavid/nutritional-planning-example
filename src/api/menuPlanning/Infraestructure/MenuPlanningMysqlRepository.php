d0b9f18c91aec9edea9fe8987883786be4d88b73
David Palacios <dpalacios@getindya.com>
2024-03-15 18:01:49 +0100

    Comment about ORMs

--- src/api/menuPlanning/Infraestructure/MenuPlanningMysqlRepository.php
+++ src/api/menuPlanning/Infraestructure/MenuPlanningMysqlRepository.php
@@ -5,6 +5,12 @@ namespace api\menuPlanning\Infraestructure;
 use api\menuPlanning\Domain\MenuPlanning;
 use api\menuPlanning\Domain\MenuPlanningRepositoryInterface;
 
+/*
+ * IMPORTANT: Using a framework, we would obviously use the ORM here (Doctrine, Eloquent...) to handle the persistence.
+ * Example with Doctrine:
+ *  class DoctrineMenuPlanningRepository extends DoctrineRepository implements MenuPlanningRepositoryInterface
+ *
+ */
 class MenuPlanningMysqlRepository implements MenuPlanningRepositoryInterface
 {
     public function findLastActiveByAthleteId(int $athleteId): ?MenuPlanning
